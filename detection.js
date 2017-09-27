b4f9050a5657856054be3d3bc8deb2f90b99dd4c
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-09-26 23:27:50 -0400

    Clean the code, make displayArray() draw on geo, change arr[] for points[]

--- detection.js
+++ detection.js
@@ -1,14 +1,8 @@
-var sum = 0;
-
 function dotDetection() {
-    // console.log("got none!");
     for (var i = 0; i < 2500; i++) {
         var x = Math.floor(sketch.random(0, sketch.width));
         var y = Math.floor(sketch.random(0, sketch.height));
-        // geo.ellipse(x, y, 5);
-        // console.log(sketch.pixels[(x + y * sketch.width) * 4]);
         if (sketch.pixels[(x + y * sketch.width) * 4] >= 100) {
-            // console.log("got one!");
             if (points.length != 0) {
                 var tooClose = false;
                 for (var j = 0; j < points.length; j++) {
@@ -19,11 +13,9 @@ function dotDetection() {
                     }
                 }
                 if (!tooClose) {
-                    sum++;
                     points.push({ x: x, y: y });
                 }
             } else {
-                sum++;
                 points.push({ x: x, y: y });
             }
         }
@@ -32,15 +24,12 @@ function dotDetection() {
 
 function displayArray() {
     if (points.length) {
-        // sketch.push();
-        // sketch.scale(0.25, 0.25);
         for (var j = 0; j < points.length; j++) {
             if (points[j]) {
-                sketch.fill(255);
-                sketch.ellipse(points[j].x - sketch.width / 2, points[j].y - sketch.height / 2, 5);
+                geo.fill(255);
+                geo.ellipse(points[j].x - geo.width / 2, points[j].y - geo.height / 2, 5);
             }
         }
-        // sketch.pop();
     }
     pointsDisplay.html(points.length);
 }
