ee2d042fdeae997e0819e5f80e414c128bd7d427
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-09-27 14:29:06 -0400

    Remove obsolete behaviour methods

--- systems.js
+++ systems.js
@@ -15,18 +15,11 @@ function loadSystems() {
             maxForce: 10,
             desiredSeparation: 150
         },
-        behaviour: function(that) {
-            for (let i = 0; i < this.graph.length; i++) {
-                this.graph[i].x += Math.random() - 0.5;
-                this.graph[i].y += Math.random() - 0.5;
-            }
-        },
         graph: fetchJSON("wed-sep-27-2017-014236")
     });
     firstSystem.addFlock({
         type: "static",
         color: [255, 0, 0],
-        behaviour: function() {},
         graph: fetchJSON("wed-sep-27-2017-014506")
     });
     firstSystem.flocks[0].addAttractors(firstSystem.flocks[1], 0.0015);
